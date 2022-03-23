583647a11783e2d4c68a46628a677876d3985142
Mansvini <coderspecial@gmail.com>
2022-03-23 17:26:34 +0530

    prettier

--- src/components/CounterButton2.js
+++ src/components/CounterButton2.js
@@ -10,13 +10,9 @@ class CounterButton extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
-      return;
-
-      true;
+      return true;
     }
-    return;
-
-    false;
+    return false;
   }
 
   updateCount = () => {
