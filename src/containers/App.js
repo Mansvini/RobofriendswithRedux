ed5d36f9510efdbad7cccfcb2d15632b5bb795e3
Mansvini <coderspecial@gmail.com>
2022-04-05 22:33:06 +0530

    updating

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     return (
       <div className="tc">
         <a
-          class="github-fork-ribbon right-top"
+          className="github-fork-ribbon right-top"
           href="https://github.com/Mansvini/RobofriendswithRedux"
           target="_blank"
           rel="noreferrer"
